788db20999fc63e6bb4db978ea5ffa8a04263d1e
Lukas Ruebbelke <simpul@gmail.com>
2014-07-17 15:13:06 -0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,119 @@
+var app = angular.module('website', ['ngAnimate']);
+
+app.controller('MainCtrl', function ($scope, $timeout) {
+    var INTERVAL = 3000;
+
+    function setCurrentSlideIndex(index) {
+        $scope.currentIndex = index;
+    }
+
+    function isCurrentSlideIndex(index) {
+        return $scope.currentIndex === index;
+    }
+
+    function nextSlide() {
+        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        $timeout(nextSlide, INTERVAL);
+    }
+
+    function setCurrentAnimation(animation) {
+        $scope.currentAnimation = animation;
+    }
+
+    function isCurrentAnimation(animation) {
+        return $scope.currentAnimation === animation;
+    }
+
+    $scope.slides = [
+        {image: 'images/image00.jpg', description: 'Image 00'},
+        {image: 'images/image01.jpg', description: 'Image 01'},
+        {image: 'images/image02.jpg', description: 'Image 02'},
+        {image: 'images/image03.jpg', description: 'Image 03'},
+        {image: 'images/image04.jpg', description: 'Image 04'}
+    ];
+
+    $scope.currentIndex = 0;
+    $scope.currentAnimation = 'slide-left-animation';
+
+    $scope.setCurrentSlideIndex = setCurrentSlideIndex;
+    $scope.isCurrentSlideIndex = isCurrentSlideIndex;
+    $scope.setCurrentAnimation = setCurrentAnimation;
+    $scope.isCurrentAnimation = isCurrentAnimation;
+
+    $timeout(nextSlide, INTERVAL);
+});
+
+app.animation('.slide-left-animation', function ($window) {
+    return {
+        enter: function (element, done) {
+            TweenMax.fromTo(element, 1, { left: $window.innerWidth}, {left: 0, onComplete: done});
+        },
+
+        leave: function (element, done) {
+            TweenMax.to(element, 1, {left: -$window.innerWidth, onComplete: done});
+        }
+    };
+});
+
+app.animation('.slide-down-animation', function ($window) {
+    return {
+        enter: function (element, done) {
+            TweenMax.fromTo(element, 1, { top: -$window.innerHeight}, {top: 0, onComplete: done});
+        },
+
+        leave: function (element, done) {
+            TweenMax.to(element, 1, {top: $window.innerHeight, onComplete: done});
+        }
+    };
+});
+
+app.animation('.fade-in-animation', function ($window) {
+    return {
+        enter: function (element, done) {
+            TweenMax.fromTo(element, 1, { opacity: 0}, {opacity: 1, onComplete: done});
+        },
+
+        leave: function (element, done) {
+            TweenMax.to(element, 1, {opacity: 0, onComplete: done});
+        }
+    };
+});
+
+
+app.directive('bgImage', function ($window, $timeout) {
+    return function (scope, element, attrs) {
+        var resizeBG = function () {
+            var bgwidth = element.width();
+            var bgheight = element.height();
+
+            var winwidth = $window.innerWidth;
+            var winheight = $window.innerHeight;
+
+            var widthratio = winwidth / bgwidth;
+            var heightratio = winheight / bgheight;
+
+            var widthdiff = heightratio * bgwidth;
+            var heightdiff = widthratio * bgheight;
+
+            if (heightdiff > winheight) {
+                element.css({
+                    width: winwidth + 'px',
+                    height: heightdiff + 'px'
+                });
+            } else {
+                element.css({
+                    width: widthdiff + 'px',
+                    height: winheight + 'px'
+                });
+            }
+        };
+
+        var windowElement = angular.element($window);
+        windowElement.resize(resizeBG);
+
+        element.bind('load', function () {
+            resizeBG();
+        });
+    }
+});
+

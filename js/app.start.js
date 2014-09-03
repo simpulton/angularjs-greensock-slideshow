3617761e0f3a7d7b50886f865ea4e9b3dbb0931e
Lukas Ruebbelke <simpul@gmail.com>
2014-09-03 07:33:29 -0700

    Adding starting files

---
+++ js/app.start.js
@@ -0,0 +1,26 @@
+var app = angular.module('website', ['ngAnimate', 'ui.bootstrap']);
+
+app.controller('MainCtrl', function ($scope, $timeout, QueueService) {
+
+});
+
+app.factory('QueueService', function($rootScope){
+
+});
+
+app.animation('.slide-left-animation', function ($window) {
+
+});
+
+app.animation('.slide-down-animation', function ($window) {
+
+});
+
+app.animation('.fade-in-animation', function ($window) {
+
+});
+
+app.directive('bgImage', function ($window, $timeout) {
+
+});
+
